68f7df8207db02892fb6da2c56a996a80892646d
Vyankatesh Madgundi <35259008+venkydmadgundi@users.noreply.github.com>
2025-10-18 17:42:56 +0530

    Enhance add method to support new line in numbers
    Updated the add method to handle new line characters in input.

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,12 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
+    numbers_array = numbers.gsub("\n", ",")
+    numbers_array.split(",").map(&:to_i).sum
   end
 end
 
 calculator = StringCalculator.new
 puts calculator.add("")
+puts calculator.add("1,2,3")
+puts calculator.add("1\n2,3,4")
