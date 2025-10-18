d1bec5702e98932c60e63a60cf822c55d0590738
Vyankatesh Madgundi <35259008+venkydmadgundi@users.noreply.github.com>
2025-10-18 18:00:27 +0530

    Improve add method for custom delimiters
    Enhance the add method to support custom delimiters and new line handling.

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,17 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    numbers_array = numbers.gsub("\n", ",")
-    numbers_array.split(",").map(&:to_i).sum
+    delimiter_new_line = "\n"
+    if numbers.start_with?('//')
+      delimiter_part, numbers_string = numbers.split("\n", 2)
+      delimiter = delimiter_part[2..-1]
+      numbers_string = numbers.gsub(delimiter_new_line, delimiter)
+      numbers_array = numbers_string.split(delimiter).map(&:to_i)
+    else
+      numbers_string = numbers.gsub(delimiter_new_line, ",")
+      numbers_array = numbers_string.split(",").map(&:to_i)
+    end
+    numbers_array.sum
   end
 end
 
@@ -10,3 +19,5 @@ calculator = StringCalculator.new
 puts calculator.add("")
 puts calculator.add("1,2,3")
 puts calculator.add("1\n2,3,4")
+puts calculator.add("//;\n1;2")
+puts calculator.add("//;\n1;2;3;5\n6")
